67049a47468d3d7c3562d936eadf2c432eacba8d
Ireh-Jo <kingslanding@naver.com>
2021-03-30 00:16:56 +0900

    Router설정

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,14 @@
-import "./App.css";
+import { HashRouter as Router, Route } from "react-router-dom";
+import Detail from "../routes/Detail";
+import Home from "../routes/Home";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <Route exact path="/" component={Home} />
+      <Route path="/:id" component={Detail} />
+    </Router>
+  );
 }
 
 export default App;
